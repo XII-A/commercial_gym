876955ca598a8785f8f28b5a0fe5188fd8c41f24
Abdulrazak <abdulrazakarmnazi@gmail.com>
2023-11-07 13:37:55 +0300

    fix: added colors correctly to tailwind config

--- gym_website/tailwind.config.js
+++ gym_website/tailwind.config.js
@@ -12,10 +12,10 @@ module.exports = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
-    },
-    color: {
-      primary: "#097FD9",
-      white: "#FFFFFF",
+      colors: {
+        primary: "#097FD9",
+        white: "#FFFFFF",
+      },
     },
   },
   plugins: [],
