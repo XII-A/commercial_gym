cfcc2d52b5c6c98014c32be6b559e0d829d0b60c
Abdulrazak <abdulrazakarmnazi@gmail.com>
2023-11-07 13:31:50 +0300

    feat: Added colors to tailwaind config file

--- gym_website/src/app/page.js
+++ gym_website/src/app/page.js
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2 bg-slate-600">
+    <div className="flex flex-col items-center justify-center min-h-screen py-2 ">
       Hello World
     </div>
   );
